5a3466a053c88dd57123b0cd71543c3cfe53be8b
andrewstagnaro <42977270+andrewstagnaro@users.noreply.github.com>
2018-10-26 13:34:20 -0400

    Add files via upload

---
+++ DiceArray/src/DiceArray.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author astagnaro20
+ * Creates 2 die, rolls them 1000 times, counts # of times each number was rolled
+ * Pd 6
+ * 
+ */
+public class DiceArray 
+{
+	public static void main(String[] args)
+	{
+		//Fields
+	Dice die1 = new Dice();
+	Dice die2 = new Dice();
+	int [] count = new int[13];
+	}
+	
+
+
+
+
+}
