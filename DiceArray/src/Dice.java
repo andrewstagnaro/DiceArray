504b87162b3e42deee0c71bd9fe37be338c05aa7
andrewstagnaro <42977270+andrewstagnaro@users.noreply.github.com>
2018-10-29 13:27:50 -0400

    Add files via upload

--- DiceArray/src/Dice.java
+++ DiceArray/src/Dice.java
@@ -1,4 +1,49 @@
-
-public class Dice {
-
+/**
+ * 
+ * @author Drew Stagnaro
+ * Dice Class
+ * Pd 6
+ *
+ */
+public class Dice 
+{
+	//Fields
+	
+	private int numRolls;
+	
+	//Constructors
+	/**
+	 * @param sets numRolls as 0
+	 */
+	public Dice()
+	{
+		numRolls = 0;
+	}
+	
+	//Methods
+	/**
+	 * Rolls the dice and adds 1 to number of rolls each time the method is called
+	 * @return roll 	returns what number the dice rolled
+	 */
+	public int roll()
+	{
+	 numRolls++;
+	 int roll = (int)(Math.random()*6) + 1;
+	 return roll;
+	}
+	/**
+	 * resets the number of rolls
+	 */
+	public void reset()
+	{
+		numRolls = 0;
+	}
+	/**
+	 * returns the number of times the dice was rolled
+	 * @return number of rolls
+	 */
+	public int getNumRolls()
+	{
+		return numRolls;
+	}
 }
--- DiceArray/src/DiceArray.java
+++ DiceArray/src/DiceArray.java
@@ -9,14 +9,21 @@ public class DiceArray
 {
 	public static void main(String[] args)
 	{
-		//Fields
 	Dice die1 = new Dice();
 	Dice die2 = new Dice();
+	
 	int [] count = new int[13];
+	for(int i = 0; i<1000; i++)
+	{
+		int sum = die1.roll() + die2.roll();
+		count[sum]++;
+		
+	}
+	for(int i = 2; i<13; i++)
+	{
+		System.out.println("You rolled " + count[i] + " " + i + "'s.");
 	}
 	
-
-
-
-
+	}
+	
 }
